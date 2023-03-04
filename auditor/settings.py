7d7f19356398a2e335ad44b2916fb19f27d66d4e
Sodimu Gbemiga <sodimuoluwagbemiga@gmail.com>
2023-03-04 15:30:10 +0100

    more changes

--- auditor/settings.py
+++ auditor/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -142,6 +143,9 @@ STATICFILES_DIRS = [
 MEDIA_ROOT = BASE_DIR/ 'static/img/bread images'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+WHITENOISE_MAX_AGE = 0
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
